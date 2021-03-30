eae06bf59aad8d13e25ce0f5de5aa555e281a1be
Milos <milosleng@gmail.com>
2021-03-30 20:11:05 +0200

    Add converting function1

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-//input controller
+// ## User Stories
+
+// -   [ ] User can enter up to 8 binary digits in one input field
+// -   [ ] User must be notified if anything other than a 0 or 1 was entered
+// -   [ ] User views the results in a single output field containing the decimal (base 10) equivalent of the binary number that was entered
+
+//selects elements
 const binaryTextArea = document.getElementById('binary');
 const decimalTextArea = document.getElementById('decimal');
-console.log(binaryTextArea, decimalTextArea);
+const button = document.getElementById('convert');
 
 //control binary area
-binary.addEventListener('keydown', (event) => {
+
+binaryTextArea.addEventListener('keydown', (event) => {
   if (event.keyCode === 8 || event.key.match(/[0-1]/g)) {
-    return event;
+    return Number(binaryTextArea.value);
   } else {
-    event.preventDefault();
+    event.preventDefault() + alert("You can enter only 1's and 0's!");
   }
 });
 
 //control decimal area
 decimalTextArea.addEventListener('keydown', (event) => {
-  if (event.keyCode === 8 || event.key.match(/[0-9]|/g)) {
-    return event;
+  if (event.keyCode === 8 || event.key.match(/[0-9]/g)) {
+    return Number(decimalTextArea.value);
   } else {
-    event.preventDefault();
+    event.preventDefault() +
+      alert('You can enter any number, but not letters or special signs!');
   }
 });
+
+button.addEventListener('click', () => {});
+
+//dec to binary
+function decToBinary(n) {
+  const toArr = n.toString().split('');
+  let newArr = [];
+  for (let i = 0; i < toArr.length; i++) {
+    if (Number(toArr[i]) === 1) {
+      newArr.push(Math.pow(2, toArr.length - (i + 1)));
+    }
+  }
+  return newArr.reduce((curr, total) => curr + total);
+}
+
+console.log(decToBinary(1010));
+
+//binary to dec fn
+function binaryToDec(num) {
+  const toArr = num.toString().split('');
+  let newArr = [];
+  for (let i = 0; i < toArr.length; i++) {
+    if (Number(toArr[i]) === 1) {
+      newArr.push(Math.pow(2, toArr.length - (i + 1)));
+    }
+  }
+  return newArr.reduce((curr, total) => curr + total);
+}
+console.log(binaryToDec(1000110));
+
+//dec to binary fn
 
 //check if you can convert negative values too!
 
